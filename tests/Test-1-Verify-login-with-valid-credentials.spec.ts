02aa3d11d5c9afd1102142a4e899019216b79e09
OleksandrMoldovan <alexmoldovan1027@gmail.com>
2025-12-11 12:50:09 +0200

    Added test

---
+++ tests/Test-1-Verify-login-with-valid-credentials.spec.ts
@@ -0,0 +1,25 @@
+import { expect, test } from "@playwright/test"
+
+
+
+test('Verify login with valid credentials', async ({ page }) => {
+  const emailLocator = '#email'
+  const passwordLocator = '#password'
+  const email = 'customer@practicesoftwaretesting.com'
+  const password = 'welcome01'
+  const submitLocator = '.btnSubmit'
+  const titleLocator = `h1[data-test = 'page-title']`
+
+
+  await page.goto('/auth/login');
+  await page.locator(emailLocator).fill(email)
+  await page.locator(passwordLocator).fill(password)
+  await page.locator(submitLocator).click()
+
+  await expect(page).toHaveURL('https://practicesoftwaretesting.com/account')
+
+  await expect(page.locator(titleLocator)).toHaveText('My account')
+
+  //await expect(page.getByRole('heading',{ name: 'My account' })).toBeVisible();
+  await expect(page.locator('a#menu')).toHaveText('Jane Doe')
+})
